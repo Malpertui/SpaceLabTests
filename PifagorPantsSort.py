acc356b40f671bb49cad19bb4829162cf1cb5da1
Malpertui <malpertui@gmail.com>
2024-06-07 02:56:54 +0300

    4th variant

--- PifagorPantsSort.py
+++ PifagorPantsSort.py
@@ -4,10 +4,7 @@ for i in range(3):
     number = int(input('Введіть число: '))
     numbers_list.append(number)
 
-
-numbers_list.sort()
-
-    
+numbers_list.sort()    
 
 if numbers_list[0]**2 + numbers_list[1]**2 == numbers_list[2]**2:
     print("True")
